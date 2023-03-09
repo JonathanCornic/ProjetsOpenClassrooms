094d706bd1b1cbc82ee0547dcfc7aea9fd0b0729
Jonathan Cornic <117487357+JonathanCornic@users.noreply.github.com>
2023-03-09 20:39:17 +0100

    Add files via upload

--- projet 3/Portfolio-architecte-sophie-bluel/FrontEnd/scripts/homepage.js
+++ projet 3/Portfolio-architecte-sophie-bluel/FrontEnd/scripts/homepage.js
@@ -1,9 +1,9 @@
 // appel et stockage du fetch
 const reponse = await fetch("http://localhost:5678/api/works");
 const projets = await reponse.json();
-if(reponse.ok){
+if (reponse.ok) {
     console.log(projets);
-}else{
+} else {
     throw new Error("echec lors de l'appel API.");
 }
 
@@ -15,95 +15,75 @@ const sectionGallery = document.createElement("div");
 sectionGallery.className = "gallery";
 
 // fonction pour recuperer tous les projets
-function genererProjets(projets){
-    projets.forEach(element => {
-        
+function genererProjets(projets) {
+    projets.forEach((element) => {
         const carteProjet = document.createElement("figure");
         const imageProjet = document.createElement("img");
         imageProjet.src = element.imageUrl;
         const titreProjet = document.createElement("figcaption");
         titreProjet.innerHTML = element.title;
-        
+
         portfolio.appendChild(sectionGallery);
         sectionGallery.appendChild(carteProjet);
         carteProjet.appendChild(imageProjet);
         carteProjet.appendChild(titreProjet);
-        
     });
-    
 }
 genererProjets(projets);
 
 // selection conteneur filtre
-const conteneurFiltre = document.querySelector(".conteneurBtnFiltre").style.display="flex";
+const conteneurFiltre = (document.querySelector(
+    ".conteneurBtnFiltre"
+).style.display = "flex");
 
 // selection des boutons de filtre
 const filtreTous = document.querySelector(".btnTous");
 const filtreObjets = document.querySelector(".btnObjets");
 const filtreAppartements = document.querySelector(".btnAppartements");
-const filtreHotelsEtRestaurants = document.querySelector(".btnHotelsEtRestaurants");
+const filtreHotelsEtRestaurants = document.querySelector(
+    ".btnHotelsEtRestaurants"
+);
 
 // fonction pour le visuel du bouton actif
 function couleurFiltre(bouton, click) {
-    const boutons = document.querySelectorAll("button"); 
-    boutons.forEach(function(b){
-        b.style.color="#1D6154";
-        b.style.backgroundColor="#FFFEF8";
+    const boutons = document.querySelectorAll("button");
+    boutons.forEach(function (b) {
+        b.style.color = "#1D6154";
+        b.style.backgroundColor = "#FFFEF8";
     });
-    if(click === "true"){
-        bouton.style.color="#FFFEF8";
-        bouton.style.backgroundColor="#1D6154";
+    if (click === "true") {
+        bouton.style.color = "#FFFEF8";
+        bouton.style.backgroundColor = "#1D6154";
     }
 }
 
 // gestion des filtres
-filtreTous.addEventListener("click", function(){
+filtreTous.addEventListener("click", function () {
     document.querySelector(".gallery").innerHTML = "";
-    genererProjets(projets); 
-    couleurFiltre(filtreTous, "true") ; 
+    genererProjets(projets);
+    couleurFiltre(filtreTous, "true");
 });
-filtreObjets.addEventListener("click", function(){
-    const filtre = projets.filter(function(element){
-        return element.category.name === "Objets"
+filtreObjets.addEventListener("click", function () {
+    const filtre = projets.filter(function (element) {
+        return element.category.name === "Objets";
     });
     document.querySelector(".gallery").innerHTML = "";
     genererProjets(filtre);
     couleurFiltre(filtreObjets, "true");
 });
-filtreAppartements.addEventListener("click", function(){
-    const filtre = projets.filter(function(element){
-        return element.category.name === "Appartements"
+filtreAppartements.addEventListener("click", function () {
+    const filtre = projets.filter(function (element) {
+        return element.category.name === "Appartements";
     });
     document.querySelector(".gallery").innerHTML = "";
     genererProjets(filtre);
     couleurFiltre(filtreAppartements, "true");
 });
-filtreHotelsEtRestaurants.addEventListener("click", function(){
-    const filtre = projets.filter(function(element){
-        return element.category.name === "Hotels & restaurants"
+filtreHotelsEtRestaurants.addEventListener("click", function () {
+    const filtre = projets.filter(function (element) {
+        return element.category.name === "Hotels & restaurants";
     });
     document.querySelector(".gallery").innerHTML = "";
     genererProjets(filtre);
     couleurFiltre(filtreHotelsEtRestaurants, "true");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- projet 3/Portfolio-architecte-sophie-bluel/FrontEnd/scripts/login.js
+++ projet 3/Portfolio-architecte-sophie-bluel/FrontEnd/scripts/login.js
@@ -1,34 +1,28 @@
-const form = document.getElementById('form');
-localStorage.setItem("admin", false)
+const form = document.getElementById("form");
+localStorage.setItem("admin", false);
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
 
-    fetch('http://localhost:5678/api/users/login',{
-      	method: 'POST',
-      	body: JSON.stringify({ email, password }),
-      	headers: {
-        	'Content-Type': 'application/json'
-      	}
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: {
+            "Content-Type": "application/json",
+        },
     })
-    .then(reponse =>  reponse.json())
-    .then(data => {
-        if(data.userId === 1){
-            localStorage.setItem("admin", true);
-            localStorage.setItem("token", data.token);
-            window.location.href = "index.html";
-        }else{
-            confirm("Erreur dans l’identifiant ou le mot de passe")
-        }
-    })
-    .catch(error => console.error(error));
+        .then((reponse) => reponse.json())
+        .then((data) => {
+            if (data.userId === 1) {
+                localStorage.setItem("admin", true);
+                localStorage.setItem("token", data.token);
+                window.location.href = "index.html";
+            } else {
+                confirm("Erreur dans l’identifiant ou le mot de passe");
+            }
+        })
+        .catch((error) => console.error(error));
 });
-
-        
-        
-  
-    
-
